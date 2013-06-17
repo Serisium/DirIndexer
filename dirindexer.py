36af8d87f7b63c074f79b69dcdb663ac3f09cf12
Garrett Greenwood <garrettagreenwood@gmail.com>
2013-06-17 11:27:23 -0500

    Added exclusion of filetypes

--- dirindexer.py
+++ dirindexer.py
@@ -1,58 +1,87 @@
-'''
-Created on May 28, 2013
-
-@author: david
-'''
-from whoosh import fields
 from whoosh.index import create_in
-from datetime import datetime
+from whoosh.index import open_dir
+from whoosh import highlight
+from whoosh.fields import *
 import os
-from whoosh.qparser import QueryParser
+import argparse
 
-
-class DirSchema(fields.SchemaClass):
-    file_name = fields.ID(stored=True)
-    content = fields.TEXT
-    path = fields.ID(stored=True)
-    indexed_on = fields.DATETIME(stored=True)
-
-
-def index(dir_nm):
-	schema = DirSchema()
+def get_ix():
+	schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT, date=STORED)
+	#create if not exists
 	if not os.path.exists(".indexdir"):
 		os.mkdir(".indexdir")
+		create_in(".indexdir", schema)
 
-	ix = create_in(".indexdir", schema)
-	w = ix.writer()
-	for root, sub_folder, files in os.walk(dir_nm):
-		for cur_file in files:
-			cur_file = open(cur_file)
-			print "indexing %s" % cur_file.name
+	ix = open_dir(".indexdir")
+	return ix
 
-			#items to index
-			content = u"%s" % cur_file.read()
-			file_name = u"%s" % cur_file.name
-			path = u"%s" % os.path.join(
-                           root, cur_file.name)
+def index(args):
+	try:
+		ix = get_ix()
+		writer = ix.writer()
+		dir_nm = removeNonAscii(args.directory)
+		for root, sub_folders, files in os.walk(dir_nm):
+			files = [f for f in files if not f[0] == '.']
+			sub_folders[:] = [d for d in sub_folders if not d[0] == '.'] #os.walk will not process deleted directories
 
-			w.add_document(file_name=file_name,
-                           content=content,
-                           path=path,
-                           indexed_on=datetime.now())
-	w.commit()
+			files = [f for f in files if not os.path.splitext(f)[1][1:] in args.exclude]
+			
+			for cur_file in files:
+				
+				fullpath = u"%s" % os.path.join(root, cur_file)
+				#fullpath =  u"%s" % root + "/"  + cur_file
+				cur_file = open(fullpath)
+				print "indexing %s" % cur_file.name
+				#items to index
+				content = u"%s" % removeNonAscii(cur_file.read())
+				file_name = u"%s" % cur_file.name
+				path = u"%s" % os.path.join(root, cur_file.name)
+				modtime = os.path.getmtime(path)
+				writer.add_document(title=file_name, path=fullpath, content=content, date=modtime)
+	finally:
+		writer.commit()
+		ix.close()
 
-def search(term):
-	schema = DirSchema()
-	ix = create_in(".indexdir", schema)
-	with ix.searcher() as searcher:
-		query = QueryParser("content", ix.schema).parse(term)
-		results = searcher.search(query)
-		print results
-		for result in results:
-			print result
+
+def search(args):
+	try:
+		ix = get_ix()
+		search_term = removeNonAscii(args.keyword)
+	
+		from whoosh.qparser import QueryParser
+		with ix.searcher() as searcher:
+			query = QueryParser("content", ix.schema).parse(u"%s" % search_term)
+			results = searcher.search(query, terms=True)
+			print results
+			results.fragmenter = highlight.ContextFragmenter(maxchars = 300, surround = 50, charlimit = 1000000)
+			results.order = highlight.SCORE
+			for i, result in enumerate(results):
+				print "Result " + str(i) + ": " + result["path"]
+				
+				i+=1
+				with open(result["path"]) as f:
+					file_content = u"%s" % removeNonAscii(f.read())
+				print result.highlights("content", text=file_content, top=10)
+				print "\n"
+	finally:
+		ix.close()
+
+def removeNonAscii(s):
+	return "".join(i for i in s if ord(i)<128)
 
 if __name__ == '__main__':
-    dir_nm = "/home/david/Developer/repos/dirindexer/files"
-    index(dir_nm)
-    term = u"schema"
-    search(term)
+	parser = argparse.ArgumentParser(description='index a directory or search for keywords.')
+	subparsers = parser.add_subparsers()
+
+	parser_index = subparsers.add_parser('index', help="index a given directory for future searches")
+	parser_index.add_argument('directory', help="the directory to search")
+	parser_index.set_defaults(func=index)
+	parser_index.add_argument("-x", "--exclude", nargs='+', help="Exclude specified filetypes from index")
+
+	parser_search = subparsers.add_parser('search', help="search the indexed directory for a keyword")
+	parser_search.add_argument('keyword', help="the search term")
+	parser_search.set_defaults(func=search)
+	
+	args=parser.parse_args()
+	args.func(args)
+
