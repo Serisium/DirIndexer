3a9acaaa20da8e47eee03c4e00ee23a5ea4902d3
Garrett Greenwood <garrettagreenwood@gmail.com>
2013-06-27 14:13:14 -0500

    Refactored code into a class

--- dirindexer.py
+++ dirindexer.py
@@ -16,181 +16,203 @@ import time
 import logging
 import Queue
 
-def get_ix():
-	schema = Schema(title=TEXT(stored=True), path=ID(stored=True,unique=True), content=TEXT, date=STORED)
-	#create if not exists
-	if not os.path.exists(".indexdir"):
-		os.mkdir(".indexdir")
-		create_in(".indexdir", schema)
 
-	ix = open_dir(".indexdir")
-	return ix
+class DirIndexer:
+	def __init__(self, args):
+		if args.func == DirIndexer.index or args.func == DirIndexer.update or args.func == DirIndexer.daemon:
+			self.directory = args.directory
+			self.exclude = args.exclude
+			self.include = args.include
+			self.processors = args.processors
+			self.all = args.all
+		
+		if args.func == DirIndexer.search:
+			self.keyword = args.keyword
+			self.color = args.color
+			self.limit = args.limit
+			self.exclude = args.exclude
+			self.include = args.include
+			
+		if args.func == DirIndexer.clear:
+			pass
+		
+	def get_ix(self):
+		schema = Schema(title=TEXT(stored=True), path=ID(stored=True,unique=True), content=TEXT, date=STORED)
+		#create if not exists
+		if not os.path.exists(".indexdir"):
+			os.mkdir(".indexdir")
+			create_in(".indexdir", schema)
 
-def get_cores(args):
-	cores = multiprocessing.cpu_count()/2
-	if args.processors:
-		cores = args.processors
+		ix = open_dir(".indexdir")
+		return ix
 
-	return cores
+	def get_cores(self):
+		cores = multiprocessing.cpu_count()/2
+		if self.processors:
+			cores = self.processors
 
-def scan_directory(dir_nm, writer, all, exclude, include, checknew=False, to_index=set(), indexed_paths=set()):
-	x = 0
-	print dir_nm
-	for root, sub_folders, files in os.walk(dir_nm, followlinks=True):
-		#Remove hidden files
-		if not all:
-			files = [f for f in files if not f[0] == '.']
-			sub_folders[:] = [d for d in sub_folders if not d[0] == '.'] #os.walk will not process deleted directories
+		return cores
 
-		#Remove excluded files
-		if exclude:
-			files = [f for f in files if not os.path.splitext(f)[1][1:] in exclude]
-		if include:
-			files = [f for f in files if os.path.splitext(f)[1][1:] in include]
+	def scan_directory(self, dir_nm, writer, all, exclude, include, checknew=False, to_index=set(), indexed_paths=set()):
+		x = 0
+		print dir_nm
+		for root, sub_folders, files in os.walk(dir_nm, followlinks=True):
+			#Remove hidden files
+			if not all:
+				files = [f for f in files if not f[0] == '.']
+				sub_folders[:] = [d for d in sub_folders if not d[0] == '.'] #os.walk will not process deleted directories
 
-		for cur_file in files:
-			path = unicode(os.path.join(root, cur_file))
+			#Remove excluded files
+			if exclude:
+				files = [f for f in files if not os.path.splitext(f)[1][1:] in exclude]
+			if include:
+				files = [f for f in files if os.path.splitext(f)[1][1:] in include]
 
-			#If a file is in the queue or is new, add it
-			if checknew and path in to_index or path not in indexed_paths:
-				add_doc(writer, path)
-				x += 1
-			elif not checknew:
-				add_doc(writer, path)
-				x += 1
-	return x
+			for cur_file in files:
+				path = unicode(os.path.join(root, cur_file))
 
-def index(args):
-	try:
-		ix = get_ix()
+				#If a file is in the queue or is new, add it
+				if checknew and path in to_index or path not in indexed_paths:
+					self.add_doc(writer, path)
+					x += 1
+				elif not checknew:
+					self.add_doc(writer, path)
+					x += 1
+		return x
+
+	def index(self):
+	#try:
+		ix = self.get_ix()
 
 		#get the number of cores to use
-		procs = get_cores(args)
+		procs = self.get_cores()
 
 		writer = ix.writer(procs=procs)
-		dir_nm = unicode(args.directory)
+		dir_nm = unicode(self.directory)
 
 		#recursively scan the directory and add files
-		x = scan_directory(dir_nm, writer, args.all, args.exclude, args.include)
-	finally:
+		x = self.scan_directory(dir_nm, writer, self.all, self.exclude, self.include)
+	#finally:
 		print "Writing %d files to index" % x
 		writer.commit()
 		ix.close()
 
 
-def update(args):
-	try:
-		ix = get_ix()
+	def update(self):
+		try:
+			ix = self.get_ix()
 
-		#get the number of cores to use
-		procs = get_cores(args)
+			#get the number of cores to use
+			procs = self.get_cores()
 
-		writer = ix.writer(procs=procs)
-		indexed_paths = set()	#Holds all paths already indexed
-		to_index = set()	#Holds a list of paths to index later
+			writer = ix.writer(procs=procs)
+			indexed_paths = set()	#Holds all paths already indexed
+			to_index = set()	#Holds a list of paths to index later
 
-		x = 0
-		with ix.searcher() as searcher:
-			for fields in searcher.all_stored_fields():
-				indexed_path = fields['path']
-				indexed_paths.add(indexed_path)
+			x = 0
+			with ix.searcher() as searcher:
+				for fields in searcher.all_stored_fields():
+					indexed_path = fields['path']
+					indexed_paths.add(indexed_path)
 
-				#if the file has been deleted, remove it from the index
-				if not os.path.exists(indexed_path):
-					remove_doc(writer, indexed_path)
-				else:
-					indexed_time = fields['date']
-					mtime = os.path.getmtime(indexed_path)
+					#if the file has been deleted, remove it from the index
+					if not os.path.exists(indexed_path):
+						self.remove_doc(writer, indexed_path)
+					else:
+						indexed_time = fields['date']
+						mtime = os.path.getmtime(indexed_path)
 
-					#If file has been changed, delete it and add it to the queue
-					if mtime > indexed_time:
-						remove_doc(writer, indexed_path)
-						to_index.add(indexed_path)
+						#If file has been changed, delete it and add it to the queue
+						if mtime > indexed_time:
+							self.remove_doc(writer, indexed_path)
+							to_index.add(indexed_path)
 
-			x = scan_directory(args.directory, writer, args.all, args.exclude, args.include, True, to_index, indexed_paths)
+				x = self.scan_directory(self.directory, writer, self.all, self.exclude, self.include, True, to_index, indexed_paths)
 
-	finally:
-		print "Writing " + str(x) + " files to index."
-		writer.commit()
-		ix.close()
+		finally:
+			print "Writing " + str(x) + " files to index."
+			writer.commit()
+			ix.close()
 
-def daemon(args):
-	ix = get_ix()
-	writer=ix.writer()
-	event_handler = IndexWriterEventHandler(writer, args.all, args.exclude, args.include)
-	observer = Observer()
-	observer.schedule(event_handler, path=args.directory, recursive=True)
-	observer.start()
-	#observer.should_keep_running()
-	
-	
-	
-	try:
-		while True:
-			time.sleep(1)
-	except KeyboardInterrupt:
-		observer.stop()
-		writer.commit()
-		ix.close()
-	observer.join()
+	def daemon(self):
+		ix = self.get_ix()
+		writer=ix.writer()
+		q = Queue.Queue()
+		event_handler = IndexWriterEventHandler(writer, self, self.all, self.exclude, self.include)
+		observer = Observer()
+		observer.schedule(event_handler, path=self.directory, recursive=True)
+		observer.start()
+		#observer.should_keep_running()	
+		
+		try:
+			while True:
+				time.sleep(1)
+		except KeyboardInterrupt:
+			observer.stop()
+			writer.commit()
+			ix.close()
+		observer.join()
 
-def add_doc(writer, path):
-	cur_file = codecs.open(path, encoding = 'utf-8', errors='ignore')
-	content = cur_file.read()
-	file_name = unicode(cur_file.name)
-	path = unicode(path)
-	modtime = os.path.getmtime(path)
-	print "Indexing %s" % file_name
-	writer.add_document(title=file_name, path=path, content=content, date=modtime)
+	def queue_timer(self, q):
+		pass	
 
-def remove_doc(writer, path):
-	print "Removing %s" % path
-	writer.delete_by_term('path', path)
+	def add_doc(self, writer, path):
+		cur_file = codecs.open(path, encoding = 'utf-8', errors='ignore')
+		content = cur_file.read()
+		file_name = unicode(cur_file.name)
+		path = unicode(path)
+		modtime = os.path.getmtime(path)
+		print "Indexing %s" % file_name
+		writer.add_document(title=file_name, path=path, content=content, date=modtime)
 
-def search(args):
-	try:
-		ix = get_ix()
-		search_term = unicode(args.keyword)
+	def remove_doc(self, writer, path):
+		print "Removing %s" % path
+		writer.delete_by_term('path', path)
 
-		from whoosh.qparser import QueryParser
-		with ix.searcher() as searcher:
-			query = QueryParser("content", ix.schema).parse(u"%s" % search_term)
-			results = searcher.search(query, terms=True, limit=args.limit)
-			results.fragmenter = highlight.ContextFragmenter(maxchars=200, surround=20)
+	def search(self):
+		try:
+			ix = self.get_ix()
+			search_term = unicode(self.keyword)
 
-			#If stdin == stdout, the programs output is not being piped and colored output is fine
-			color  = (args.color == 'always' or (args.color == 'auto' and os.fstat(0) == os.fstat(1)))
-			results.formatter = ColorFormatter(color=color)
+			from whoosh.qparser import QueryParser
+			with ix.searcher() as searcher:
+				query = QueryParser("content", ix.schema).parse(u"%s" % search_term)
+				results = searcher.search(query, terms=True, limit=self.limit)
+				results.fragmenter = highlight.ContextFragmenter(maxchars=200, surround=20)
 
-			#Remove excluded filetypes from search results
-			if args.exclude:
-				results = [f for f in results if not os.path.splitext(f["path"])[1][1:] in args.exclude]
-			if args.include:
-				results = [f for f in results if os.path.splitext(f["path"])[1][1:] in args.include]
+				#If stdin == stdout, the programs output is not being piped and colored output is fine
+				color  = (self.color == 'always' or (self.color == 'auto' and os.fstat(0) == os.fstat(1)))
+				results.formatter = ColorFormatter(color=color)
 
-			print results
-			for i, result in enumerate(results):
-				if color:
-					print "Result %i: " % i + colorama.Fore.GREEN + result["path"] + colorama.Fore.RESET
-				else:
-					print "Result %i: %s" % (i, result["path"])
-				with codecs.open(result["path"], encoding='utf-8', errors='ignore') as f:
-					file_content = f.read()
-					print result.highlights("content", text=file_content, top=10)
-					print "\n"
-	finally:
-		ix.close()
+				#Remove excluded filetypes from search results
+				if self.exclude:
+					results = [f for f in results if not os.path.splitext(f["path"])[1][1:] in self.exclude]
+				if self.include:
+					results = [f for f in results if os.path.splitext(f["path"])[1][1:] in self.include]
+
+				print results
+				for i, result in enumerate(results):
+					if color:
+						print "Result %i: " % i + colorama.Fore.GREEN + result["path"] + colorama.Fore.RESET
+					else:
+						print "Result %i: %s" % (i, result["path"])
+					with codecs.open(result["path"], encoding='utf-8', errors='ignore') as f:
+						file_content = f.read()
+						print result.highlights("content", text=file_content, top=10)
+						print "\n"
+		finally:
+			ix.close()
 
 
-def clear(args):
-	print "Deleting the current index..."
-	for root, dirs, files in os.walk(os.getcwd()+"/.indexdir/", topdown=False):
-		for name in files:
-			print name
-			os.remove(os.path.join(root,name))
-		for name in dirs:
-			os.rmdir(os.path.join(root,name))
-	os.rmdir(os.getcwd()+"/.indexdir/")
+	def clear(self):
+		print "Deleting the current index..."
+		for root, dirs, files in os.walk(os.getcwd()+"/.indexdir/", topdown=False):
+			for name in files:
+				print name
+				os.remove(os.path.join(root,name))
+			for name in dirs:
+				os.rmdir(os.path.join(root,name))
+		os.rmdir(os.getcwd()+"/.indexdir/")
 
 class ColorFormatter(highlight.Formatter):
 	#Formatter for seach output
@@ -206,13 +228,21 @@ class ColorFormatter(highlight.Formatter):
 			return tokentext
 
 class IndexWriterEventHandler(FileSystemEventHandler):
-	def __init__(self, writer, all=False, exclude = [], include = None):
+	def __init__(self, writer, di, all=False, exclude = [], include = None):
 		self.writer = writer
+		self.di=di
 		self.all = all
 		self.exclude = exclude
 		self.include = include
-		print("Init")		
-
+	
+	#def dispatch(self, event):
+	#	self.on_any_event(event)
+	#	self.q.put(event)
+	
+	def clear_queue(self):
+		while not self.q.empty():
+			return FileSystemEventHandler.dispatch(self, self.q.get())
+	
 	def pathIsGood(self, path):
 		if not self.all:
 			if os.path.basename(path)[0] == '.':
@@ -228,22 +258,22 @@ class IndexWriterEventHandler(FileSystemEventHandler):
 	def on_created(self, event):
 		print("on_create")
 		if self.pathIsGood(event.src_path):
-			add_doc(self.writer, event.src_path)
+			self.di.add_doc(self.writer, event.src_path)
 	def on_moved(self, event):
 		print("on_moved")
-		remove_doc(self.writer, event.src_path)
+		self.di.remove_doc(self.writer, event.src_path)
 		if self.pathIsGood(event.dest_path):
-			add_doc(self.writer, event.dest_path)
+			self.di.add_doc(self.writer, event.dest_path)
 	
 	def on_deleted(self, event):
 		print("on_deleted")
-		remove_doc(self.writer, event.src_path)
+		self.di.remove_doc(self.writer, event.src_path)
 
 	def on_modified(self, event):
 		print("on_modified")
 		if self.pathIsGood(event.src_path):
-			remove_doc(self.writer, event.src_path)
-			add_doc(self.writer, event.src_path)
+			self.di.remove_doc(self.writer, event.src_path)
+			self.di.add_doc(self.writer, event.src_path)
 		
 class IndexTask:
 	def __init__(self, eventType):
@@ -251,6 +281,7 @@ class IndexTask:
 
 
 def start():
+	
 	colorama.init();
 	
 	parser = argparse.ArgumentParser(description='index a directory or search for keywords.')
@@ -258,7 +289,7 @@ def start():
 
 	parser_index = subparsers.add_parser('index', help="Index a given directory for future searches")
 	parser_index.add_argument('directory', help="the directory to search")
-	parser_index.set_defaults(func=index)
+	parser_index.set_defaults(func=DirIndexer.index)
 	parser_index_filegroup = parser_index.add_mutually_exclusive_group()
 	parser_index_filegroup.add_argument("-x", "--exclude", nargs='+', help="Exclude specified filetypes from index")
 	parser_index_filegroup.add_argument("-i", "--include", nargs='+', help="Include only the specified filetypes in the index")
@@ -267,7 +298,7 @@ def start():
 
 	parser_update = subparsers.add_parser('update',help="Update the index with new or edited files")
 	parser_update.add_argument('directory', help="The directory to update")
-	parser_update.set_defaults(func=update)
+	parser_update.set_defaults(func=DirIndexer.update)
 	parser_update_filegroup = parser_update.add_mutually_exclusive_group()
 	parser_update_filegroup.add_argument("-x", "--exclude", nargs='+', help="Exclude specified filetypes from update")
 	parser_update_filegroup.add_argument("-i", "--include", nargs='+', help="Include only the specified filetypes in the update")
@@ -276,15 +307,16 @@ def start():
 
 	parser_daemon = subparsers.add_parser('daemon', help="Start a daemon to automatically update the index.")
 	parser_daemon.add_argument('directory', help="The directory to watch.")
-	parser_daemon.set_defaults(func=daemon)
+	parser_daemon.set_defaults(func=DirIndexer.daemon)
 	parser_daemon_filegroup = parser_daemon.add_mutually_exclusive_group()
 	parser_daemon_filegroup.add_argument("-x", "--exclude", nargs='+', help="Exclude the specified filetypes from the updates.")
 	parser_daemon_filegroup.add_argument("-i", "--include", nargs='+', help="Include only the specified filetypes in the updates.")
+	parser_daemon.add_argument("-p", "--processors", type=int, help="Number of processors to utilize")
 	parser_daemon.add_argument("-a", "--all", action='store_true', help="Include hidden files and directories in the update.")
 
 	parser_search = subparsers.add_parser('search', help="Search the indexed directory for a keyword")
 	parser_search.add_argument('keyword', help="the search term")
-	parser_search.set_defaults(func=search)
+	parser_search.set_defaults(func=DirIndexer.search)
 	parser_search.add_argument('-c', '--color', choices=['auto','always','never'], default='auto', type=str, help="Highlight the search term")
 	parser_search.add_argument('-l', '--limit', type=int, default=None, help='Number of results to show; passing None will show all')
 	parser_search_filegroup = parser_search.add_mutually_exclusive_group()
@@ -292,10 +324,13 @@ def start():
 	parser_search_filegroup.add_argument('-i', "--include", nargs='+', help="Include only the specified filetypes in the search")
 	
 	parser_clear = subparsers.add_parser("clear", help="Delete the current index.")
-	parser_clear.set_defaults(func=clear)
+	parser_clear.set_defaults(func=DirIndexer.clear)
 
 	args = parser.parse_args()
-	args.func(args)
+	
+	di = DirIndexer(args)
+	
+	args.func(di)
 
 if __name__ == '__main__':
     start()
